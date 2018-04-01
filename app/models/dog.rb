0d5938b30e17f6d4d78d6763535dbb4b8782d9f2
Jay Bednar <ironviralmedia@gmail.com>
2018-04-01 19:37:19 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,20 +1,20 @@
 class Dog
-  # attr_accessor :name, :breed, :age
-  #
-  # @@all = []
-  #
-  # def self.all
-  #   @@all
-  # end
-  #
-  # def save
-  #   @@all << self
-  # end
-  #
-  # def initialize(name: , breed: , age:)
-  #   @name = name
-  #   @breed = breed
-  #   @age = age
-  #   self.save
-  # end
+  attr_accessor :name, :breed, :age
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def initialize(name: , breed: , age:)
+    @name = name
+    @breed = breed
+    @age = age
+    self.save
+  end
 end
